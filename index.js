ac7f1fde15352e80e5926efa6f710faa13e38c40
ningunickdisponible <albruni309@hotmail.com>
2021-09-10 23:44:43 -0300

    heroku changes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
- console.log("El servidor está inicializado en el puerto 3000");
-});
+app.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
     console.log('test');
