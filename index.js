aefc64370f6eaf8c59d7710a1bdb4a0a62967a59
ningunickdisponible <albruni309@hotmail.com>
2021-09-10 23:47:46 -0300

    enable CORS

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 app.listen(process.env.PORT || 3000);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
     console.log('test');
     res.send('Saludos desde express');
